36f4f40f97a06b2bd9750ecce0249f014f615fcb
Michael Ries <michael@riesd.com>
2014-03-17 22:58:26 -0600

    adding game model

---
+++ models/game.rb
@@ -0,0 +1,33 @@
+class Score
+  include Virtus.model
+
+  attribute :id, Integer
+  attribute :player_id, Integer
+  attribute :hole01, Integer
+  attribute :hole02, Integer
+  attribute :hole03, Integer
+  attribute :hole04, Integer
+  attribute :hole05, Integer
+  attribute :hole06, Integer
+  attribute :hole07, Integer
+  attribute :hole08, Integer
+  attribute :hole09, Integer
+  attribute :hole10, Integer
+  attribute :hole11, Integer
+  attribute :hole12, Integer
+  attribute :hole13, Integer
+  attribute :hole14, Integer
+  attribute :hole15, Integer
+  attribute :hole16, Integer
+  attribute :hole17, Integer
+  attribute :hole18, Integer
+end
+
+class Game
+  include Virtus.model
+
+  attribute :id, Integer
+  attribute :course_id, Integer
+  attribute :played_at, DateTime
+  attribute :scores, Array[Score]
+end

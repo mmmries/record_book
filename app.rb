c134e46e4f0222552c11a7d7452c2789667474f5
Michael Ries <michael@riesd.com>
2014-03-17 23:19:50 -0600

    got the parsing of a game working

--- app.rb
+++ app.rb
@@ -1,11 +1,19 @@
 require 'bundler/setup'
 Bundler.require
 
-require_relative "course_record"
+require_relative "models/course_record"
+require_relative "models/game"
 
 Rabl.register!
 
 get "/:course_id/records.json" do
   @courses = CourseRecord.where(:course_id => params[:course_id])
   rabl :records
+end
+
+post "/game.json" do
+  game = Game.new(JSON.parse(request.body.read))
+
+  status 200
+  body ""
 end
